d95e5143a1bc7846d85eac0c0018c9e9be8fa6b1
Jenartyz <167534735+Jenartyz@users.noreply.github.com>
2026-05-13 23:50:31 +0800

    Added login feature using new db

--- Student-Evaluation/cli_modules/EvaluationModule/obj/Debug/net8.0/EvaluationModule.AssemblyInfo.cs
+++ Student-Evaluation/cli_modules/EvaluationModule/obj/Debug/net8.0/EvaluationModule.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvaluationModule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a883d91ca431ae2f49715f0031baa4cc5fe94748")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf56cf94f987a94cd3b1f38ca9a37910ddeab9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvaluationModule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvaluationModule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
